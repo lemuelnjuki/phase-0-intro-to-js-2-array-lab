19df749321d2518ff58dbf08324cd7acce436978
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-05 11:09:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift("Milo");
+}
+function appendCat() {
+    let LemCats = [...cats];
+    LemCats.push("Broom");
+    return LemCats;
+}
+function prependCat() {
+    let LemCats =[...cats];
+    LemCats.unshift("Arnold");
+    return LemCats;
+}
+function removeLastCat() {
+    let LemCats =[...cats];
+    LemCats.pop();
+    return LemCats;
+}
+function  removeFirstCat() {
+    let LemCats =[...cats];
+    LemCats.shift();
+    return LemCats;
+}
